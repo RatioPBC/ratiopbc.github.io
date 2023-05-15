e6d0789fc5607911f25c29e7b34ed42fe11a7767
Kenichi Nakamura <kenichi.nakamura@gmail.com>
2023-05-15 15:30:45 -0700

    slightly bigger logos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ export function Footer() {
     <footer className="bg-slate-50">
       <Container>
         <div className="py-16">
-          <Logo className="mx-auto h-10 w-auto" />
+          <Logo className="mx-auto h-24 w-auto" />
           <nav className="mt-10 text-sm" aria-label="quick links">
           </nav>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -96,7 +96,7 @@ export function Header() {
         <nav className="relative z-50 flex justify-between">
           <div className="flex items-center md:gap-x-12">
             <Link href="#" aria-label="Home">
-              <Logo className="h-10 w-auto" />
+              <Logo className="h-20 w-auto" />
             </Link>
             <div className="hidden md:flex md:gap-x-6">
               <NavLink href="#features">Blog</NavLink>
